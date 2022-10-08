d4afa6b32bdec0676c921030cc1a4323b3cc4edb
Alexis Santa Ana <ibanez2112@hotmail.com>
2022-10-08 00:28:49 -0300

    fixed data objects

--- src/screens/character/index.js
+++ src/screens/character/index.js
@@ -4,7 +4,7 @@ import { styles } from "./styles";
 import { useSelector } from "react-redux";
 
 const Character = () => {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   const character = useSelector((state) => state.characters.selected);
 
@@ -27,20 +27,21 @@ const Character = () => {
             <Text style={styles.text}>Name: {character.name}</Text>
             <Text style={styles.text}>Height: {character.height}</Text>
             <Text style={styles.text}>Gender: {character.gender}</Text>
+            {character.bornLocation && <Text style={styles.text}>Born location: {character.bornLocation}</Text>}
             {character.homeworld && <Text style={styles.text}>Homeworld: {character.homeworld}</Text>}
-            {character.died && <Text style={styles.text}>Death location: {character.died}</Text>}
-            {character.hair && <Text style={styles.text}>Hair: {character.hair}</Text>}
-            {character.eyes && <Text style={styles.text}>Eyes: {character.eyes}</Text>}
-            {character.skin && <Text style={styles.text}>Skin: {character.skin}</Text>}
+            {character.diedLocation && <Text style={styles.text}>Death location: {character.diedLocation}</Text>}
+            {character.hairColor && <Text style={styles.text}>Hair: {character.hairColor}</Text>}
+            {character.eyesColor && <Text style={styles.text}>Eyes: {character.eyesColor}</Text>}
+            {character.skinColor && <Text style={styles.text}>Skin: {character.skinColor}</Text>}
             {character.cybernetics && <Text style={styles.text}>Cybernetics: {character.cybernetics}</Text>}
             {character.creator && <Text style={styles.text}>Creator: {character.creator}</Text>}
             {character.manufacturer && <Text style={styles.text}>Manufacturer: {character.manufacturer}</Text>}
             {character.model && <Text style={styles.text}>Model: {character.model}</Text>}
-            {character.plating && <Text style={styles.text}>Plating: {character.plating}</Text>}
+            {character.platingColor && <Text style={styles.text}>Plating: {character.platingColor}</Text>}
             {character.masters && <Text style={styles.text}>Masters: {character.masters}</Text>}
             {character.apprentices && <Text style={styles.text}>Apprentices: {character.apprentices}</Text>}
-            {character.classs && <Text style={styles.text}>Class: {character.classs}</Text>}
-            {character.sensor && <Text style={styles.text}>Sensor: {character.sensor}</Text>}
+            {character.class && <Text style={styles.text}>Class: {character.class}</Text>}
+            {character.sensorColor && <Text style={styles.text}>Sensor: {character.sensorColor}</Text>}
           </>
         )}
       </View>
